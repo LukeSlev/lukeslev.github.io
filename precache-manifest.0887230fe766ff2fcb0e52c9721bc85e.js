6c7fe04584a3c33db1bbca6d056a73e95400ea3f
LukeSlev <lslevins@ualberta.ca>
2019-01-22 18:17:38 -0700

    Updates

---
+++ precache-manifest.0887230fe766ff2fcb0e52c9721bc85e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14fb83bd1d89b5c4955",
+    "url": "/static/css/main.e26b7d09.chunk.css"
+  },
+  {
+    "revision": "e14fb83bd1d89b5c4955",
+    "url": "/static/js/main.e14fb83b.chunk.js"
+  },
+  {
+    "revision": "3f62bccaf502698fc58d",
+    "url": "/static/css/1.bea3271d.chunk.css"
+  },
+  {
+    "revision": "3f62bccaf502698fc58d",
+    "url": "/static/js/1.3f62bcca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85f9bb194f7ebbd837b45741cd888c08",
+    "url": "/index.html"
+  }
+];
